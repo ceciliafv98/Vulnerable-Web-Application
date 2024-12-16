7c3bc337f3ce95cdde5acf7babcc757552f6400d
ceciliafv98 <yzp14903@educastur.es>
2024-12-16 19:21:40 +0100

    Update lvl4.php
    Se sanitiza el código, verificando que se permite la introducción de archivos de forma segura

--- FileInclusion/pages/lvl4.php
+++ FileInclusion/pages/lvl4.php
@@ -17,25 +17,51 @@
       <a href=lvl4.php?file=1.php><button>Button</button></a>
       <a href=lvl4.php?file=2.php><button>The Other Button!</button></a>
       </div>
-      
-      <?php     
-        echo "</br></br>";
+      <?php
+echo "</br></br>";
 
-        if (isset( $_GET[ 'file' ])) 
-        {
-          $secure4 = $_GET[ 'file' ];
+// Verificar si 'file' está presente en la URL
+if (isset($_GET['file'])) {
+    
+    // Obtener el nombre del archivo de manera segura
+    $secure4 = $_GET['file'];
+    
+    // Lista de archivos permitidos
+    $allowed_files = ['archivo1.php', 'archivo2.php']; // Solo permitimos estos archivos
+    
+    // Verificamos si el archivo solicitado está en la lista de permitidos
+    if (in_array($secure4, $allowed_files)) {
+        
+        // Incluir el archivo
+        include($secure4);
+
+        // Mostrar el nombre del archivo de manera segura (prevención de XSS)
+        echo "<div align='center'><b><h5>" . htmlspecialchars($secure4, ENT_QUOTES, 'UTF-8') . "</h5></b></div>";
+    } else {
+        // Si el archivo no está permitido, mostrar un mensaje de error
+        echo "Este archivo no está permitido.";
+    }
+}
+?>
+
+    //  <?php     
+      //  echo "</br></br>";
+
+       // if (isset( $_GET[ 'file' ])) 
+      //  {
+        //  $secure4 = $_GET[ 'file' ];
          
-            if ($secure4!="1.php" && $secure4!="2.php") 
-            {
-              $secure4=substr($secure4, 0,-4);
-            }
+         //   if ($secure4!="1.php" && $secure4!="2.php") 
+         //   {
+        //      $secure4=substr($secure4, 0,-4);
+        //    }
             
-            if (isset($secure4)) 
-            {        
-              include($secure4);              
-            }
-        }              
-      ?>
+       //     if (isset($secure4)) 
+       //     {        
+        //      include($secure4);              
+        //    }
+       // }              
+     // ?>
    </body>
 </html>
 
