ca085763db773cf2d15ad58b7d20ba98a3fb1a3a
ceciliafv98 <yzp14903@educastur.es>
2024-12-17 19:38:29 +0100

    Update sql2.php
    se evita la inyección de código y se usa la función htmlspecialchars()

--- SQL/sql2.php
+++ SQL/sql2.php
@@ -22,7 +22,7 @@
 <?php
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = getenv('MYSQL_SECURE_PASSWORD');
 	$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
 	// Create connection
@@ -35,12 +35,14 @@
 	//echo "Connected successfully";
 	if(isset($_POST["submit"])){
 		$number = $_POST['number'];
-		$query = "SELECT bookname,authorname FROM books WHERE number = $number"; //Int
+		$query = "SELECT bookname,authorname FROM books WHERE number = ?"; //Int
 		$result = mysqli_query($conn,$query);
+		 mysqli_stmt_bind_param($result, "s", $firstname);
+		 mysqli_stmt_execute($result);
 
 		if (!$result) { //Check result
 		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    $message .= 'Whole query: ' . $query;
+		    $message .= 'Whole query: ' . htmlspecialchars($query);
 		    die($message);
 		}
 
