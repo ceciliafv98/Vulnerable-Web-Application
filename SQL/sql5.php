66f3907d374c6de271485967fe08c46eb31cfcaa
ceciliafv98 <yzp14903@educastur.es>
2024-12-17 20:03:33 +0100

    Update sql5.php

--- SQL/sql5.php
+++ SQL/sql5.php
@@ -25,7 +25,7 @@
 <?php
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = getenv('MYSQL_SECURE_PASSWORD');
 	$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
 	// Create connection
@@ -47,12 +47,13 @@
 			exit;
 		}
 
-		$query = "SELECT bookname,authorname FROM books WHERE number =".'$number'; 
+		$query = "SELECT bookname,authorname FROM books WHERE number = ?"; 
 		$result = mysqli_query($conn,$query);
-
+		 mysqli_stmt_bind_param($result, "i", $number);
+		 mysqli_stmt_execute($result);
 		if (!$result) { //Check result
 		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    $message .= 'Whole query: ' . $query;
+		    $message .= 'Whole query: ' . htmlspecialchars($query);
 		    die($message);
 		}
 
