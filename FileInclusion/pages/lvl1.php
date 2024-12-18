b528de843117d9764df9cae995490d9254089355
ceciliafv98 <yzp14903@educastur.es>
2024-12-18 12:39:46 +0100

    Update lvl1.php

--- FileInclusion/pages/lvl1.php
+++ FileInclusion/pages/lvl1.php
@@ -16,40 +16,28 @@
       <a href=lvl1.php?file=1.php><button>Button</button></a>
       <a href=lvl1.php?file=2.php><button>The Other Button!</button></a>
       </div>
+
+
       <?php
-echo "</br></br>";
+             $INCLUDE_ALLOW_LIST = {
+                      "home.php",
+                      "dashboard.php",
+                      "profile.php",
+                     "settings.php"
 
-// Lista de archivos permitidos
-$allowed_files = ['file1.php', 'file2.php', 'file3.php']; // Los archivos que pueden ser incluidos
-
-// Verificamos si 'file' está presente en la URL y si es un archivo permitido
-if (isset($_GET['file'])) {
-    $file = $_GET['file'];
-
-    // Verificamos que el archivo esté en la lista de archivos permitidos
-    if (in_array($file, $allowed_files)) {
+             };
+             
+        echo "</br></br>";
         
-        // Incluir el archivo de manera segura
-        @include($file);
-        
-        // Mostrar el nombre del archivo de manera segura
-        echo "<div align='center'><b><h5>" . htmlspecialchars($file, ENT_QUOTES, 'UTF-8') . "</h5></b></div>";
-    } else {
-        // Si el archivo no es permitido
-        echo "Este archivo no está permitido.";
-    }
-}
-?>
-
-     // <?php
-     //   echo "</br></br>";
-        
-    //    if (isset( $_GET[ 'file' ]))        
+       if (isset( $_GET[ 'file' ]))        
         {
-      //    @include($_GET[ 'file' ]);
-      //   echo"<div align='center'><b><h5>".$_GET[ 'file' ]."</h5></b></div> ";       
-      //  }
-    //  ?>
+           $filename = $_GET ['file'];
+         if (in_array($filename, $INCLUDE_ALLOW_LIST)){
+        @include($filename);
+       echo"<div align='center'><b><h5>".$_GET[ 'file' ]."</h5></b></div> ";       
+      }
+        }
+      ?>
    </body>
 </html>
 
